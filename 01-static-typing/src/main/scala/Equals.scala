7fa517664582a52ecaa809bfb5c7a08f61285f66
r8vnhill <reachme@ravenhill.cl>
2026-03-10 19:19:21 -0300

    :sparkles: feat(equals): implement equals function for integer comparison

--- 01-static-typing/src/main/scala/Equals.scala
+++ 01-static-typing/src/main/scala/Equals.scala
@@ -3,8 +3,9 @@ package cl.uchile.dcc
 /**
  * Compares two integers for equality.
  *
- * This function checks if the two integer values `a` and `b` are equal. Before
- * performing the comparison, it prints "equals called" to the console.
+ * This function checks if the two integer values `a` and `b` are equal.
+ * Before performing the comparison, it prints "equals called" to the
+ * console.
  *
  * @param a
  *   the first integer to compare
@@ -18,6 +19,9 @@ package cl.uchile.dcc
  *   equals(2, 5)   // prints "equals called" and returns false
  *   }}}
  */
-def equals(a: Int, b: Int): Boolean =
+def equals(
+    a: Int,
+    b: Int
+): Boolean =
   println("equals called")
   a == b
